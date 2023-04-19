50205ead52a21b5e8209704e7a32221ad73dd42c
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-04-19 11:45:45 +0300

    CustomValidator sınıfın oluşturulması

---
+++ FluentValidationApp.WebUI/FluentValidations/CustomerValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidationApp.WebUI.Models;
+
+namespace FluentValidationApp.WebUI.FluentValidations
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name alanı boş olamaz");
+            
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email alanı boş olamaz")
+                .EmailAddress().WithMessage("Email doğru formatta olmalıdır.");
+            
+            RuleFor(x => x.Age)
+                .NotEmpty().WithMessage("Yaş alanı boş olamaz")
+                .InclusiveBetween(18, 60).WithMessage("Yaş aralığını 18-60 arasında olmalıdır");
+
+        }
+    }
+}
--- FluentValidationApp.WebUI/Program.cs
+++ FluentValidationApp.WebUI/Program.cs
@@ -1,12 +1,27 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidationApp.WebUI.FluentValidations;
+using FluentValidationApp.WebUI.Models;
 using FluentValidationApp.WebUI.Models.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews().
+//Fluent iþlemlerinin gerçekleþmesi için eklenecek kod bloðu aþaððýda oluþturulmuþtue
+builder.Services.AddControllersWithViews().AddFluentValidation(options =>
+{
+    //birden fazla validation iþlemi söz konusu ise bunu kullanmak daha mantýklýdýr.
+    options.RegisterValidatorsFromAssemblyContaining<Program>();
+
+});
 ////1.Yöntem
 builder.Services.AddDbContext<AppDatabaseContext>();
+
+
+//ya da Fluent iþlemi için 1 tane için yapýlabilir ama çok sayýda varsa üstteki kodu yazmak daha effective
+//builder.Services.AddSingleton<IValidator<Customer>, CustomerValidator>();
 
 var app = builder.Build();
 
