70e53a23f0de78dcaee541420c59ca9c39f080cb
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-04-19 10:35:07 +0300

    Projenin oluşturulması database bağlantısı işlemleri

---
+++ FluentValidationApp.WebUI/Models/Context/AppDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidationApp.WebUI.Models.Context
+{
+    public class AppDatabaseContext : DbContext
+    {
+        //1.Yöntem
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data source=IGU-NB-0884;initial catalog=FluentValidationDb;user id=sa;password=s123456*-;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        //2.Yöntem
+
+        //public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options) : base(options)
+        //{
+
+        //}
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+
+}
--- FluentValidationApp.WebUI/Program.cs
+++ FluentValidationApp.WebUI/Program.cs
@@ -1,9 +1,20 @@
+using FluentValidationApp.WebUI.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+////1.Yöntem
+builder.Services.AddDbContext<AppDatabaseContext>();
 
 var app = builder.Build();
+
+////2.Yöntem
+//builder.Services.AddDbContext<AppDatabaseContext>(options =>
+//{
+//    options.UseSqlServer(app.Configuration["FluentConnectionStrings"]);
+//});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
