9aba2c644a56d904b7b4ee169a081a800661ba42
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-04-20 17:39:56 +0300

    limit sonra gelen mesaj bilgisini türkçeleştirmek için kullanılan  options.QuotaExceededResponse options.QuotaExceededMessage = "{{\"mesaj\" :\" İstek limiti aşıldı \",\"Detay\":\"İzin verilen istek sayısı  {1}'de {0} istektir . {2} sn sonra tekrar deneyiniz.\"}}";

--- RateLimitApp.API/Program.cs
+++ RateLimitApp.API/Program.cs
@@ -47,6 +47,7 @@ builder.Services.Configure<IpRateLimitOptions>(options =>
     options.IpWhitelist = new List<string> { "127.0.0.1" };
     options.RealIpHeader = "X-Real-IP"; // load balancer iþlemi için kullanýlýr
     options.EndpointWhitelist= new List<string>(){ "put:/api/product" };
+    
     options.GeneralRules = new List<RateLimitRule>
         {
             new RateLimitRule
@@ -74,6 +75,9 @@ builder.Services.Configure<IpRateLimitOptions>(options =>
             }
 
         };
+
+
+    options.QuotaExceededMessage = "{{\"mesaj\" :\" Ýstek limiti aþýldý \",\"Detay\":\"Ýzin verilen istek sayýsý  {1}'de {0} istektir . {2} sn sonra tekrar deneyiniz.\"}}";
 });
 
 
